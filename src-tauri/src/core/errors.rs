6ed1153e09f52471a1f3996faf40dee59e7f2c04
RustedBlood <nekrol242@yandex.ru>
2026-01-22 01:10:37 +0500

    error on serialize removed

--- src-tauri/src/core/errors.rs
+++ src-tauri/src/core/errors.rs
@@ -1,3 +1,4 @@
+use serde::Serialize;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -6,4 +7,14 @@ pub enum CoreErrors {
     NoPermission,
     #[error("IO error {0}")]
     Io(#[from] std::io::Error),
+}
+
+impl Serialize for CoreErrors {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        // Serialize the error as a string for the frontend
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
